b69286277238e37ea6f8d5286fde2f33405054d8
tamirsit <tamir.sit@gmail.com>
2025-11-05 14:06:42 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -14,10 +14,6 @@ def find_max_number(num1, num2, num3):
    else:
       return num3
     
-    import math
-
-def find_mean_std(num1, num2, num3):
+   def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    sum_sq_diff = ((num1 - mean)**2 + (num2 - mean)**2 + (num3 - mean)**2)
-    std = math.sqrt(sum_sq_diff / 3)
     return mean, std
